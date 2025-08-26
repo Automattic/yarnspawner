12ce83d3610c18c37558718db435596010de4ea8
Igor Khrol <khroliz@gmail.com>
2025-08-26 19:39:22 +0300

    Python 3.12 support - versioneer

--- yarnspawner/__init__.py
+++ yarnspawner/__init__.py
@@ -4,6 +4,5 @@ from yarnspawner.spawner import YarnSpawner
 from . import apihandler
 del apihandler
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
